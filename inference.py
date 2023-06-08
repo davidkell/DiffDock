46aa1dee1575aa9d7fd0949235b34943d83ea053
David Kell <davidambrosekell@gmail.com>
2023-06-08 15:50:56 +0100

    Update inference.py for multiple devices

--- inference.py
+++ inference.py
@@ -47,6 +47,9 @@ parser.add_argument('--num_workers', type=int, default=1, help='Number of worker
 parser.add_argument('--sigma_schedule', type=str, default='expbeta', help='')
 parser.add_argument('--actual_steps', type=int, default=None, help='Number of denoising steps that are actually performed')
 parser.add_argument('--keep_local_structures', action='store_true', default=False, help='Keeps the local structure when specifying an input with 3D coordinates instead of generating them with RDKit')
+
+parser.add_argument('--device', type=str, default='cpu', help='Device for inference')
+
 args = parser.parse_args()
 if args.config:
     config_dict = yaml.load(args.config, Loader=yaml.FullLoader)
@@ -67,7 +70,7 @@ if args.confidence_model_dir is not None:
     with open(f'{args.confidence_model_dir}/model_parameters.yml') as f:
         confidence_args = Namespace(**yaml.full_load(f))
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device(args.device or ('cuda' if torch.cuda.is_available() else 'cpu'))
 
 if args.protein_ligand_csv is not None:
     df = pd.read_csv(args.protein_ligand_csv)
